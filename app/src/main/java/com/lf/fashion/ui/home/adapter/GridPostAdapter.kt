4831335a158d569f345cd9deb1e105c5697f7e5e
phliopox <91457591+phliopox@users.noreply.github.com>
2023-05-16 17:51:12 +0900

    2장씩 모아보기 추가

--- app/src/main/java/com/lf/fashion/ui/home/adapter/GridPostAdapter.kt
+++ app/src/main/java/com/lf/fashion/ui/home/adapter/GridPostAdapter.kt
@@ -3,14 +3,15 @@ package com.lf.fashion.ui.home.adapter
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.lf.fashion.TAG
 import com.lf.fashion.data.response.Post
 import com.lf.fashion.databinding.HomeGridItemBinding
 
-class GridPostAdapter : ListAdapter<Post, GridPostAdapter.GridPostViewHolder>(DefaultPostDiff()),
-    SpanCountEditBtnListener {
+class GridPostAdapter : ListAdapter<Post, GridPostAdapter.GridPostViewHolder>(DefaultPostDiff()) {
     private lateinit var binding: HomeGridItemBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridPostViewHolder {
@@ -24,17 +25,42 @@ class GridPostAdapter : ListAdapter<Post, GridPostAdapter.GridPostViewHolder>(De
     }
 
     inner class GridPostViewHolder(private val binding: HomeGridItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+        RecyclerView.ViewHolder(binding.root), SpanCountEditBtnListener {
+        private var spanCount = 2
         fun bind(post: Post) {
             //첫번째 사진을 grid 로 노출
+
             binding.photoUrl = post.photo[0].imageUrl
+
+            when (spanCount) {
+                2 -> {
+                    Log.d(
+                        TAG,
+                        "GridPostAdapter - editSpanCountClicked: !!!${binding.gridImage.layoutParams}"
+                    );
+                    val layoutParams = binding.gridImage.layoutParams
+                    layoutParams.height = 600
+                    binding.gridImage.layoutParams = layoutParams
+                    Log.d(TAG, "GridPostAdapter - editSpanCountClicked: !!!$layoutParams");
+                }
+                3 -> {
+                    val layoutParams = binding.gridImage.layoutParams
+                    layoutParams.height = 300
+                    binding.gridImage.layoutParams = layoutParams
+
+                }
+            }
             binding.executePendingBindings()
-
-
         }
+
+        override fun editSpanCountClicked(boolean: Boolean, newSpan: Int)  {
+            spanCount = newSpan
+            binding.executePendingBindings()
+        }
+
     }
 
-    override fun editSpanCountClicked(boolean: Boolean, spanCount: Int) {
+    /*override fun editSpanCountClicked(boolean: Boolean, spanCount: Int) {
         when(spanCount){
             2->{
                 Log.d(TAG, "GridPostAdapter - editSpanCountClicked: !!!${binding.gridImage.layoutParams}");
@@ -50,10 +76,10 @@ class GridPostAdapter : ListAdapter<Post, GridPostAdapter.GridPostViewHolder>(De
 
             }
         }
-    }
+    }*/
 
 }
 
 interface SpanCountEditBtnListener {
-    fun editSpanCountClicked(boolean: Boolean,spanCount : Int)
+    fun editSpanCountClicked(boolean: Boolean, spanCount: Int)
 }
