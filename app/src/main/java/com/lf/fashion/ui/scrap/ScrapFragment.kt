4d9e1e71e38bd64565667236644e5e3c389c6ac8
phliopox <91457591+phliopox@users.noreply.github.com>
2023-07-11 17:32:07 +0900

    scrap login check 추가, dialog 추가

--- app/src/main/java/com/lf/fashion/ui/scrap/ScrapFragment.kt
+++ app/src/main/java/com/lf/fashion/ui/scrap/ScrapFragment.kt
@@ -1,17 +1,22 @@
 package com.lf.fashion.ui.scrap
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.lf.fashion.R
 import com.lf.fashion.TAG
+import com.lf.fashion.data.common.PreferenceManager
 import com.lf.fashion.data.network.Resource
 import com.lf.fashion.data.response.Posts
 import com.lf.fashion.data.response.RandomPostResponse
@@ -21,6 +26,8 @@ import com.lf.fashion.ui.GridPostAdapter
 import com.lf.fashion.ui.OnScrollUtils
 import com.lf.fashion.ui.home.GridSpaceItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.first
 
 //TODO swipe refresh 추가
 
@@ -32,6 +39,7 @@ class ScrapFragment : Fragment(), GridPhotoClickListener {
     private lateinit var gridPostAdapter: GridPostAdapter
     private lateinit var recentResponse: RandomPostResponse
     private lateinit var onScrollListener: NestedScrollView.OnScrollChangeListener
+    private lateinit var userPref: PreferenceManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,14 +52,35 @@ class ScrapFragment : Fragment(), GridPhotoClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        gridPostAdapter = GridPostAdapter(3, this@ScrapFragment, scrapPage = true)
+        userPref = PreferenceManager(requireContext())
 
-        //스크롤 리스너 설정
-        onScrollListener = OnScrollUtils { loadMorePost() }
-        binding.myNestedScrollView.setOnScrollChangeListener(onScrollListener)
+        val requestAuthKey: Deferred<String> =
+            CoroutineScope(Dispatchers.IO).async {
+                userPref.accessToken.first() ?: ""
+            }
 
-        viewModel.postResponse.observe(viewLifecycleOwner) {
-           resources ->
+        val authKey = runBlocking { requestAuthKey.await() }
+
+        val loginDialog = AlertDialog.Builder(requireContext())
+            .setMessage("로그인 후 이용가능합니다.")
+            .setPositiveButton("로그인하러 가기") { _, _ ->
+                val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavBar)
+                val loginMenuItem = bottomNavigationView.menu.findItem(R.id.navigation_mypage)
+                loginMenuItem.isChecked = true
+                bottomNavigationView.selectedItemId = R.id.navigation_mypage
+            }
+            .setNegativeButton("닫기"){_,_->
+                findNavController().navigateUp()
+            }
+
+        if (authKey.isNotEmpty()) {
+            gridPostAdapter = GridPostAdapter(3, this@ScrapFragment, scrapPage = true)
+
+            //스크롤 리스너 설정
+            onScrollListener = OnScrollUtils { loadMorePost() }
+            binding.myNestedScrollView.setOnScrollChangeListener(onScrollListener)
+
+            viewModel.postResponse.observe(viewLifecycleOwner) { resources ->
                 when (resources) {
                     is Resource.Success -> {
                         val response = resources.value
@@ -79,8 +108,7 @@ class ScrapFragment : Fragment(), GridPhotoClickListener {
                     }
                     is Resource.Failure -> {
                         if (resources.errorCode == 401) {
-                            Toast.makeText(requireContext(), "로그인 후 이용가능합니다.", Toast.LENGTH_SHORT)
-                                .show()
+                            loginDialog.show()
                         } else {
                             Log.e(TAG, "onViewCreated postList response Error: $resources ")
                         }
@@ -92,7 +120,12 @@ class ScrapFragment : Fragment(), GridPhotoClickListener {
                 }
 
 
+            }
+        } else {
+            loginDialog.show()
+
         }
+
     }
 
     private fun loadMorePost() {
@@ -114,18 +147,20 @@ class ScrapFragment : Fragment(), GridPhotoClickListener {
                         is Resource.Loading -> {
 
                         }
-                        else ->{
+                        else -> {
 
 
-                        }                        }
+                        }
                     }
                 }
             }
         }
-        override fun gridPhotoClicked(postIndex: Int) {
-            Log.d(TAG, "ScrapFragment - gridPhotoClicked: grid 포토 클릭 $postIndex")
-            // post list 에서 클릭한 포토의 포지션을 viewModel 에 저장
-            viewModel.editClickedPostIndex(postIndex)
-            findNavController().navigate(R.id.action_navigation_scrap_to_scrapVerticalFragment)
-        }
     }
+
+    override fun gridPhotoClicked(postIndex: Int) {
+        Log.d(TAG, "ScrapFragment - gridPhotoClicked: grid 포토 클릭 $postIndex")
+        // post list 에서 클릭한 포토의 포지션을 viewModel 에 저장
+        viewModel.editClickedPostIndex(postIndex)
+        findNavController().navigate(R.id.action_navigation_scrap_to_scrapVerticalFragment)
+    }
+}
