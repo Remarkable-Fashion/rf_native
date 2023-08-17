039aae104aecc327edbe0520ae01631b7a130aca
phliopox <91457591+phliopox@users.noreply.github.com>
2023-08-17 12:10:41 +0900

    팔로우,팔로잉,차단유저 내부 검색 구현 x 버튼 패딩 추가

--- app/src/main/java/com/lf/fashion/ui/mypage/followDetail/FollowDetailItemFragment.kt
+++ app/src/main/java/com/lf/fashion/ui/mypage/followDetail/FollowDetailItemFragment.kt
@@ -1,15 +1,31 @@
 package com.lf.fashion.ui.mypage.followDetail
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import com.lf.fashion.R
+import com.lf.fashion.TAG
 import com.lf.fashion.data.network.Resource
+import com.lf.fashion.data.response.OtherUser
 import com.lf.fashion.databinding.MypageFollowViewpagerBinding
 import com.lf.fashion.ui.mypage.MyPageViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FollowDetailItemFragment(private val tabName: String) :
@@ -17,6 +33,7 @@ class FollowDetailItemFragment(private val tabName: String) :
     private lateinit var binding: MypageFollowViewpagerBinding
     private val viewModel: MyPageViewModel by hiltNavGraphViewModels(R.id.navigation_mypage)
     private val userListAdapter = UserListAdapter()
+    private lateinit var currentUserList: List<OtherUser>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = MypageFollowViewpagerBinding.bind(view)
@@ -30,6 +47,7 @@ class FollowDetailItemFragment(private val tabName: String) :
                     if (resources is Resource.Success) {
                         val followers = resources.value.followers
                         updateEmptyText(followers.isNullOrEmpty(), "팔로워가 없습니다.")
+                        followers?.let { currentUserList = it }
                         userListAdapter.submitList(followers)
                     }
                 }
@@ -42,6 +60,7 @@ class FollowDetailItemFragment(private val tabName: String) :
                     if (resources is Resource.Success) {
                         val followings = resources.value.followings
                         updateEmptyText(followings.isNullOrEmpty(), "팔로잉이 없습니다.")
+                        followings?.let  { currentUserList = it }
                         userListAdapter.submitList(followings)
                     }
                 }
@@ -53,10 +72,26 @@ class FollowDetailItemFragment(private val tabName: String) :
                     if (resources is Resource.Success) {
                         val blockedUsers = resources.value.blockedUsers
                         updateEmptyText(blockedUsers.isNullOrEmpty(), "차단하신 유저가 없습니다.")
+                        blockedUsers?.let  { currentUserList = it }
                         userListAdapter.submitList(blockedUsers)
                     }
                 }
             }
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val searchEditText = binding.searchEt
+            val editTextToFlow = editTextToFlow(searchEditText)
+            editTextToFlow.debounce(500)
+               // .filter { it?.length!! > 0 }
+                .onEach {
+                    if(it?.length!! >0) {
+                        searchUser(it.toString())
+                    }else{
+                        userListAdapter.submitList(currentUserList) // 검색창 empty 시 이전 list 재전송
+                    }
+                }.launchIn(this)
+
         }
     }
 
@@ -68,4 +103,50 @@ class FollowDetailItemFragment(private val tabName: String) :
             binding.arrayEmptyText.isVisible = false
         }
     }
+
+    //Debounce 해야함
+    //observe한 list들을 상단 객체에 넣어두고 contain 으로 검색하기?
+    private suspend fun editTextToFlow(editText: EditText): Flow<CharSequence?> {
+        return callbackFlow<CharSequence?> {
+            val listener = object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) = Unit
+
+                override fun onTextChanged(
+                    text: CharSequence?,
+                    start: Int,
+                    before: Int,
+                    count: Int
+                ) {
+                    trySend(text)
+                }
+
+                override fun afterTextChanged(s: Editable?) = Unit
+
+            }
+            editText.addTextChangedListener(listener)
+            awaitClose {
+                editText.removeTextChangedListener(listener)
+            }
+
+        }.onStart {
+            Log.d(TAG, "textChangesToFlow() / onStart 발동")
+            emit(editText.text)
+        }
+    }
+
+    private fun searchUser(term: String) {
+        val resultUser = mutableListOf<OtherUser>()
+        currentUserList.forEach {
+            if (it.user.name.contains(term)) {
+                resultUser.add(it)
+            }
+        }
+        userListAdapter.submitList(resultUser)
+    }
+
 }
