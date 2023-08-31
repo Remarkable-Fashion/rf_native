2854aa7fc4f616eebb9b92d734c9ee4bdaf6b00f
phliopox <91457591+phliopox@users.noreply.github.com>
2023-08-31 16:06:03 +0900

    개인정보 처리방침,서비스약관 페이지 추가

--- app/src/main/java/com/lf/fashion/ui/mypage/setting/PrivacyPolicyFragment.kt
+++ app/src/main/java/com/lf/fashion/ui/mypage/setting/PrivacyPolicyFragment.kt
@@ -1,17 +1,12 @@
 package com.lf.fashion.ui.mypage.setting
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.lf.fashion.MainActivity
 import com.lf.fashion.R
 import com.lf.fashion.databinding.MypagePrivacyPolicyFragmentBinding
-import com.lf.fashion.databinding.MypageProfileFragmentBinding
 import com.lf.fashion.ui.getAssetsTextString
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
 
 class PrivacyPolicyFragment :Fragment(R.layout.mypage_privacy_policy_fragment) {
     private lateinit var binding:MypagePrivacyPolicyFragmentBinding
--- app/src/main/java/com/lf/fashion/ui/mypage/setting/ServiceTermFragment.kt
+++ app/src/main/java/com/lf/fashion/ui/mypage/setting/ServiceTermFragment.kt
@@ -1,18 +1,12 @@
 package com.lf.fashion.ui.mypage.setting
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.lf.fashion.MainActivity
 import com.lf.fashion.R
-import com.lf.fashion.databinding.MypagePrivacyPolicyFragmentBinding
-import com.lf.fashion.databinding.MypageProfileFragmentBinding
 import com.lf.fashion.databinding.MypageServicetermFragmentBinding
 import com.lf.fashion.ui.getAssetsTextString
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
 
 class ServiceTermFragment :Fragment(R.layout.mypage_serviceterm_fragment) {
     private lateinit var binding:MypageServicetermFragmentBinding
