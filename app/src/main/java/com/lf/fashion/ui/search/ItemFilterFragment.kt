552ca085ced9df5cb056fdd04f014febbbf8ea41
phliopox <91457591+phliopox@users.noreply.github.com>
2023-09-01 17:36:32 +0900

    아이템 필터 컬러팔레트 추가

--- app/src/main/java/com/lf/fashion/ui/search/ItemFilterFragment.kt
+++ app/src/main/java/com/lf/fashion/ui/search/ItemFilterFragment.kt
@@ -6,13 +6,12 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jaygoo.widget.OnRangeChangedListener
+import com.jaygoo.widget.RangeSeekBar
 import com.lf.fashion.R
 import com.lf.fashion.TAG
-import com.lf.fashion.databinding.HomeBPhotoFilterFragmentBinding
 import com.lf.fashion.databinding.SearchItemFilterFragmentBinding
-import com.lf.fashion.ui.addUnitTextListener
 import com.lf.fashion.ui.cancelBtnBackStack
-import com.lf.fashion.ui.childChip
 import com.lf.fashion.ui.home.frag.FilterViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,6 +23,7 @@ class ItemFilterFragment : Fragment(R.layout.search_item_filter_fragment),View.O
     private lateinit var binding: SearchItemFilterFragmentBinding
     private val viewModel: FilterViewModel by viewModels()
     private val chipStyle = "default"
+    private val priceMaxLimit = 500000
 
     override fun onResume() {
         viewModel.selectedGender?.let {
@@ -36,7 +36,7 @@ class ItemFilterFragment : Fragment(R.layout.search_item_filter_fragment),View.O
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = SearchItemFilterFragmentBinding.bind(view)
-        //생성 후 다른 바텀 메뉴 이동시 다시 home menu 클릭시 selected 아이콘으로 변경 안되는 오류 해결하기위해 수동 메뉴 checked 코드 추가
+        //생성 후 다른 바텀 메뉴 이동시 다시 home menu 클릭시 selected 아이콘으로 변경 안되는 오류 해결하기 위해 수동 메뉴 checked 코드 추가
         val bottomNavigationView =
             requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavBar)
         val homeMenu = bottomNavigationView.menu.findItem(R.id.navigation_search)
@@ -52,10 +52,32 @@ class ItemFilterFragment : Fragment(R.layout.search_item_filter_fragment),View.O
 
 
         cancelBtnBackStack(binding.cancelBtn)
-
+        priceSeekbarListener()
     }
 
+    private fun priceSeekbarListener() {
+        binding.priceSeekbar.setOnRangeChangedListener(object : OnRangeChangedListener {
+            override fun onRangeChanged(
+                view: RangeSeekBar?,
+                leftValue: Float,
+                rightValue: Float,
+                isFromUser: Boolean
+            ) {
+                val leftPrice = (priceMaxLimit / 100) * (leftValue.toInt())
+                val rightPrice = (priceMaxLimit / 100) * (rightValue.toInt())
+                binding.priceMin = leftPrice
+                binding.priceMax = rightPrice
+            }
 
+            override fun onStartTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
+            }
+
+            override fun onStopTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
+
+            }
+
+        })
+    }
 
 
     override fun onClick(v: View?) {
